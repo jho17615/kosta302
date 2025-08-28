d26a6671bc7c79509f555bed37d1665b9e033394
jho <138661240+jho17615@users.noreply.github.com>
2025-08-28 15:35:19 +0900

    원격 에서 삭제추가 bordcontoleer.java

--- src/examcontroller/bordcontoleer.java
+++ src/examcontroller/bordcontoleer.java
@@ -6,8 +6,12 @@ public class bordcontoleer {
 	public void se() {
 		System.out.println("로컬");
 	}
-public void sㅁa() {
+public void sa() {
 		System.out.println("서버에서");
+
+	}
+	public void desaeldtsa() {
+		System.out.println("서asdasd버에서");
 
 	}
 }
