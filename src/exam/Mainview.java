a8478114b3115f6dc0d722a70da66b0dee5df017
jho <jho17615@naver.com>
2025-08-28 13:48:27 +0900

    first commit

---
+++ src/exam/Mainview.java
@@ -0,0 +1,12 @@
+package exam;
+
+public class Mainview {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("시작합니다!");
+		
+		System.out.println("배고파요");
+	}
+
+}
