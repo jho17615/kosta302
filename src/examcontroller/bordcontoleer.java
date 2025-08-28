500c5eacd1f8b1768b165cf1d759bc1b33f16845
jho <jho17615@naver.com>
2025-08-28 15:44:11 +0900

    새로운 v

--- src/examcontroller/bordcontoleer.java
+++ src/examcontroller/bordcontoleer.java
@@ -10,8 +10,15 @@ public void sa() {
 		System.out.println("서버에서");
 
 	}
+
 	public void desaeldtsa() {
 		System.out.println("서asdasd버에서");
 
 	}
+
+
+public void update() {
+	System.out.println("로컬 updatae");
+}
+
 }
